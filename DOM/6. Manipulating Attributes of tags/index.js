d8828e85de55bcd73240e8c035a2b3c95506f130
Nimratjotsingh <nimratjotsingh05@gmail.com>
2025-11-18 11:54:45 +0530

    js

---
+++ DOM/6. Manipulating Attributes of tags/index.js
@@ -0,0 +1,8 @@
+// Getting info of all attributes of a tag
+document.querySelector("a").attributes;
+
+// Getting specific attribute of a tag
+document.querySelector("a").getAttribute("href");
+
+// Setting or changing attribute of a tag
+document.querySelector("a").setAttribute("href", "https://www.youtube.com/");
