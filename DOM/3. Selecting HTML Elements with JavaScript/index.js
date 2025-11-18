9d6595423d6c23e43c95653ba26c6b9dbda8c371
Nimratjotsingh <nimratjotsingh05@gmail.com>
2025-11-18 09:42:38 +0530

    js

---
+++ DOM/3. Selecting HTML Elements with JavaScript/index.js
@@ -0,0 +1,18 @@
+document.query
+
+//SelectorAll("li") {
+console.log(document.getElementsByTagName("li"));
+
+// 1. Changes the color of the third <li> element to green.
+document.querySelectorAll("li")[2].style.color = "green";
+
+// 1 & 2 both can be used to search for elements
+
+// 2. Tells the number of items in li
+console.log(document.getElementsByTagName("li").length);
+
+//This also returns array and can be used to search elements with class name, so directly we can use functions instead we have to specify
+document.getElementsByClassName("btn");
+
+// Search element by ID & manupulating it
+console.log(document.getElementById("title").innerHTML = "Goodbye");
