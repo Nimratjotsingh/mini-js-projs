a96d190e6be598f58b9cdf792368c045baa68dd5
Nimratjotsingh <nimratjotsingh05@gmail.com>
2025-11-21 19:28:24 +0530

    added animation

--- only-js-projs/object creation & constructor function/object.js
+++ only-js-projs/object creation & constructor function/object.js
@@ -1,9 +1,13 @@
 // Creating constructor Function
 
-function HouseKeeper(yearsOfExperience,name,cleaningRepertoire){
+function HouseKeeper(yearsOfExperience,name,cleaningRepertoire,clean){
     this.yearsOfExperience = yearsOfExperience;
     this.name = name;
     this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function(){
+        console.log(`Cleaning in progress by ${name}...`);
+    }
 }
 
 var houseKeeper1 = new HouseKeeper(12,"Alice",["bathroom","lobby","bedroom"]);
+houseKeeper1.clean();
