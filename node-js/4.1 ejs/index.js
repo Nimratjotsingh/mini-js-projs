812309ecec3a3cbb11512ad9c598f803141b956b
Nimratjotsingh <nimratjotsingh05@gmail.com>
2025-11-29 13:14:36 +0530

    added 4.1 ejs

---
+++ node-js/4.1 ejs/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  const data = {
+    title : "EJS Tags",
+    seconds : new Date().getSeconds(),
+    items : ["apple", "banana", "cherry"],
+    htmlContent : "<em>This is some em text</em>",
+  }
+  res.render("index.ejs",data);
+});
+
+
+app.listen(3000);
